9747b693bb1d4b6aef59bdc3ad2fe0c848c1ee68
Jahnvi Goel <jahnvigoel2003@gmail.com>
2026-02-25 13:34:09 +0530

    initial commit

---
+++ backend/src/lib/env.js
@@ -0,0 +1,9 @@
+//IS GLOBALIZED whenever we want to use we can use importing this file and using using the ENV variable.
+
+import dotenv from "dotenv";
+
+dotenv.config();
+export const ENV= {
+  PORT: process.env.PORT,
+  DB_URL:process.env.DB_URL,
+};
