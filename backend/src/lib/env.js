2a9f93d64d1d71ee256887bdf3fa112229e2a3ad
Jahnvi Goel <jahnvigoel2003@gmail.com>
2026-03-01 09:28:29 +0530

    db and tool setup

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -2,9 +2,15 @@
 
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({quiet:true});
 export const ENV= {
   PORT: process.env.PORT,
   DB_URL: process.env.DB_URL,
   NODE_ENV: process.env.NODE_ENV,
+  INNGEST_SIGNING_KEY:INNGEST_SIGNING_KEY,
+  INNGEST_EVENT_KEY:INNGEST_EVENT_KEY,
+  STREAM_API_SECRET:STREAM_API_SECRET,
+  STREAM_API_KEY:STREAM_API_KEY,
+  CLERK_PUBLISHABLE_KEY:CLERK_PUBLISHABLE_KEY,
+
 };
